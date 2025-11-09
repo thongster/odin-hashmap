91428edf8c1df6653c2aa60e9eda2c964370d449
thongster <106802301+thongster@users.noreply.github.com>
2025-11-09 22:56:36 +0700

    HashMap class and hash function

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("test")
+class HashMap {
+  constructor() {
+    this.load = 0.75; // # to determine when to grow buckets array
+    this.capacity = 16; // total # of buckets
+    this.buckets = new Array(this.capacity)
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+}
+
+const test = new HashMap()
+console.log(test)
+console.log(test.buckets)
